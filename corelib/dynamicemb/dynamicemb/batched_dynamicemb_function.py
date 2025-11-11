7bfb8d92784d48f59a3f2740e55e3c7d6f45ce34
jiashuy <jiashu.yao.cn@gmail.com>
2025-11-11 09:29:13 +0800

    Gradient clipping by reusing TorchRec&FBGEMM's parameters (#223)
    * Add gradient clipping in dynamicemb
    
    * Fix potential capacity mismatch issue in incremental_dump

--- corelib/dynamicemb/dynamicemb/batched_dynamicemb_function.py
+++ corelib/dynamicemb/dynamicemb/batched_dynamicemb_function.py
@@ -629,6 +629,10 @@ class DynamicEmbeddingFunctionV2(torch.autograd.Function):
         optimizer = ctx.optimizer
         enable_prefetch = ctx.enable_prefetch
 
+        # clip the gradient before reduction
+        if optimizer.need_gradient_clipping():
+            optimizer.clip_gradient(grads)
+
         input_dist_dedup = ctx.input_dist_dedup
         if input_dist_dedup:
             unique_indices = ctx.unique_indices
